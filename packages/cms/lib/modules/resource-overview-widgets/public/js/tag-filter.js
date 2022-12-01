65736ef523e541d0b277d017d29036bcf99cb192
LorenzoJokhan <lorenzoluk@gmail.com>
2022-12-01 10:07:01 +0100

    tagfilters are now collapsable and will open if the taglist in queryparams matches the filter list, simplified js with css adjecent rule

--- packages/cms/lib/modules/resource-overview-widgets/public/js/tag-filter.js
+++ packages/cms/lib/modules/resource-overview-widgets/public/js/tag-filter.js
@@ -2,22 +2,7 @@ var onMultiSelectToggle = function onMultiSelectToggle(filterHeaderElement) {
     var header = filterHeaderElement;
     var sibling = filterHeaderElement.nextElementSibling;
     
-    if(header && sibling.classList.contains("multi-select-children-container")) {
-        sibling.classList.toggle("multiselect-open");
-        header.classList.toggle('closed');
-
-        var childrenShouldDissapear = header.classList.contains('closed');
-        var children = sibling.childNodes;
-
-        children.forEach(function(element) {
-            if(element.nodeType === Node.ELEMENT_NODE) {
-                if(childrenShouldDissapear) {
-                    element.style.display = 'none';
-                } else {
-                    element.style.display = 'block';
-                }
-            }
-        });
-        return;
+    if(header && header.classList.contains('filter-header') && sibling.classList.contains("multi-select-children-container")) {
+        return header.classList.toggle('closed');
     }
 }
